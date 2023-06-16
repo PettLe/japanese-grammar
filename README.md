45a8489662dde384814a9068caac03e9d39278fc
Petteri Leino <petewaterz@gmail.com>
2023-06-16 12:53:37 +0300

    more content, simple fixes, readme updated and back to top button

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Test
+### Japanese Grammar
+
+Just a small project to combine several things.
+Wanted to make something simple and useful, as well as test how AI can benefit simple website building.
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// DOM must be loaded first
 $(document).ready(function() {
 data = [{title: "~てから / ~te kara / tapahtumajärjestys",
         body: `Tapahtumaketjuja voi kuvata käyttämällä verbin te-muotoa.
@@ -14,17 +15,18 @@ data = [{title: "~てから / ~te kara / tapahtumajärjestys",
         <br><i>Mainichi, uchi ni kaette kara, benkyou shimasu.</i>
         <br>Opiskelen joka päivä <strong>sen jälkeen, kun</strong> olen palannut kotiin.`},
         {title: "~てもいいですか / ~te mo ii desu ka / luvan kysyminen",
-        body: `Lupaa voidaan kysyä yhdistämällä verbin te-muodon perään partikkeli も (myös) + adjektiivi いい (hyvä) + ですか。
+        body: `Lupaa voidaan kysyä yhdistämällä verbin te-muodon perään partikkeli <strong>も</strong> (myös) + adjektiivi <strong>いい</strong> (hyvä) + 
+        <strong>ですか</strong>。
         Näin saadaan lause, jonka merkitys olisi kirjaimellisesti suurinpiirtein "vaikka tehden X, onko se hyvä?"
         <br>明日は家に<strong>行ってもいいですか</strong>。
         <br><i>Ashita wa ie ni itte mo ii desu ka.</i>
-        <br>Käykös jos tulen talollesi huomenna?
+        <br>Käykö jos tulen talollesi huomenna?
         <br>
         <br>ここで<strong>食べてもいいですか</strong>。
         <br><i>Koko de tabete mo ii desu ka.</i>
         <br>Saako täällä syödä?
         <br><br>
-        Vastaavasti luvan voi myöntää (tai kertoa mikä on ok) käyttämällä samaa rakennetta päättämättä lausetta か。
+        Vastaavasti luvan voi myöntää (tai kertoa mikä on muuten vain ok) käyttämällä samaa rakennetta yksinkertaisesti päättämättä lausetta か。
         <br>このケーキは<strong>食べてもいいです</strong>。
         <br><i>Kono keeki wa tabete mo ii desu</i>
         <br>Tämän kakun voi syödä.
@@ -33,23 +35,58 @@ data = [{title: "~てから / ~te kara / tapahtumajärjestys",
         <br><i>Ano geemu o katte mo ii yo.</i>
         <br>Voit ostaa sen pelin.`},
         {title: "~てはいけません / ~te wa ikemasen / kieltäminen",
-        body: `Kieltäminen tapahtuu lisäämällä verbin -te muodon perään はいけません
+        body: `Kieltäminen tapahtuu lisäämällä verbin -te muodon perään <strong>はいけません</strong>
         <br>レストランでタバコを<strong>吸ってはいけません</strong>。
         <br><i>Resutoran de tabako o sutte wa ikemasen.</i>
         <br>Ravintolassa ei saa polttaa tupakkaa.
 
-        <br><br>いけません on kohteliaampi muoto verbistä いけない jota myös voi käyttää. Yksinkertaisin ja kasuaalein käytettävä sana on だめ
+        <br><br>いけません on kohteliaampi muoto verbistä いけない jota myös voi käyttää. Yksinkertaisin ja kasuaalein käytettävä sana on <strong>だめ</strong>.
         <br>危ない場所に<strong>行ってはだめ</strong>よ。
         <br><i>Abunai basho ni itte wa dame yo.</i>
         <br>Vaarallisiin paikkoihin ei pidä mennä!`},
-        {title: "Testi4",
-        body: "NHAGAGAGAGAGAGtit"},
-        {title: "Testiä5",
-        body: "<br>ヤバい<br>スゲー<br>くそ"},]
+        {title: "Adjektiivien luetteleminen",
+        body: `Tapa jolla adjektiivit yhdistetään riippuu siitä ovatko ne i- vai na-adjektiiveja. Jos kyseessä on i-adjektiivi, putoaa viimeinen い pois
+        ja sen tilalle tulee ~<strong>くて</strong>. Jos kyseessä on na-adjektiivi, korvataan な hiraganalla <strong>で</strong>. Viimeistä adjektiivia ei enää 
+        taivuteta tällä tavoin. Lauseen aikamuoto ilmenee vasta lauseen lopussa!
+        <br>怖<strong>くて</strong>大きい熊です。
+        <br><i>Kowakute ookii kuma desu.</i>
+        <br>Pelottava <strong>ja</strong> iso karhu.
+        <br>
+        <br>静か<strong>で</strong>綺麗な公園です。
+        <br><i>Shizuka de kirei na kouen desu.</i>
+        <br>Rauhallinen <strong>ja</strong> kaunis puisto
+        <br>
+        <br>この車は便利<strong>で</strong>速いです。
+        <br><i>Kono kuruma wa benri de hayai desu.</i>
+        <br>Tämä auto on käytännöllinen <strong>ja</strong> nopea.
+        <br>
+        <br>Huom, lauseessa kaikkien adjektiivien on oltava merkitykseltään joko kielteisiä tai myönteisiä. Muussa tapauksessa tarvitsee käyttää sanaa mutta,
+        esim. <strong>が</strong>
+        <br>映画は面白い<strong>ですが</strong>、とても長かったです。
+        <br><i>Eiga wa omoshiroi desu ga, totemo nagakatta desu.</i>
+        <br>Elokuva <strong><i>oli</i></strong> viihdyttävä, <strong>mutta</strong> todella pitkä.`},
+        {title: "Lauseiden yhdistäminen",
+        body: `Lauseiden yhdistäminen riippuu täysin siitä, minkälainen ensimmäinen lause on. Jos kyseessä on verbi- tai i-adjektiivilause, ne yhdistetään taivuttamalla
+         ensimmäisen lauseen verbi tai adjektiivi te-muotoon. (ks. edellinen kielioppiasia)
+        <br>毎朝、新聞を<strong>読んで</strong>、ジースを飲みます。
+        <br><i>Maiasa shinbun o yonde, juusu o nomimasu.</i>
+        <br>Joka aamu luen sanomalehteä <strong>ja</strong> juon mehua.
+        <br>
+        <br>このシャツは<strong>白くて</strong>、あのシャツは黒いです。
+        <br><i>Kono shatsu wa shirokute, ano shatsu wa kuroi desu.</i>
+        <br>Tämä paita on valkoinen ja tuo paita tuolla on musta.
+        <br><br>
+        Jos kyseessä on substantiivi tai na-adjektiivi, yhdistetään lauseet laittamalla sanan perään <strong>で</strong>
+        <br>マリさんは弁護士<strong>で</strong>、マリオさんは看護師です。
+        <br><i>Mari-san wa bengoshi de, Mario-san wa kangoshi desu.</i>
+        <br>Mari on asianajaja <strong>ja</strong> Mario on sairaanhoitaja.
+        <br>
+        <br>自然が<strong>好きで</strong>、よく森を散歩します。
+        <br><i>Shizen ga suki de, yoku mori o sanpo shimasu.</i>
+        <br>Tykkään luonnosta <strong>ja</strong> käyn usein metsässä kävelyillä.
+        <br>(Huom. japanissa tykätä ei ole verbi vaan na-adjektiivi!)`},]
 
 data.forEach((item, index) => {
-    // console.log(item.title)
-    // console.log(`Tämä on esine nro ${index}`)
     let headerText = document.getElementById(`otsikko${index}`)
     headerText.textContent = item.title
 
@@ -57,7 +94,7 @@ data.forEach((item, index) => {
     infoText.innerHTML = item.body
 })
 
-//Get the button
+// Back to top button
 let mybutton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -75,7 +112,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
