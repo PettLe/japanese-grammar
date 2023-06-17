5c1ad2974d051e8f2e2ea33e3914278dbdf5c22f
Petteri Leino <petewaterz@gmail.com>
2023-06-17 08:41:13 +0300

    added instructions, fixed missing exercise and corrected typos

--- harjoitukset.js
+++ harjoitukset.js
@@ -1,4 +1,4 @@
-let harjoitukset = [{teht: `Lopussa vastaukset voi paljastaa klikkaamalla.
+let harjoitukset = [{teht: `Tehtävät on jaettu kolmeen osaan kielioppiasioiden mukaisessa järjestyksessä. Lopussa vastaukset voi paljastaa klikkaamalla.
                     <br>
                     <br>1a. 宿題を__________遊びに行きます。(verb. 終える おえる viimeistellä / saattaa päätökseen)
                     <br><i>Shukudai o __________ asobi ni ikimasu.</i>
@@ -14,12 +14,15 @@ let harjoitukset = [{teht: `Lopussa vastaukset voi paljastaa klikkaamalla.
                     <br>
                     <br>2a. 靴を__________ですよ。(脱ぐ ぬぐ riisua)
                     <br><i>Kutsu o __________ desu yo.</i>
-                    <br>Voit riisua kenkäsi
+                    <br>Voit riisua kenkäsi.
                     <br>
                     <br>2b. 写真を__________ですか。(採る とる ottaa valokuva)
                     <br><i>Shashin o __________ desu ka.</i>
                     <br>Onko ok, jos otan valokuvan?
                     <br>
+                    <br>2c. この椅子に__________ですか。(座る すわる istua)
+                    <br><i>Kono isu ni __________ desu ka.</i>
+                    <br>Voinko istua tälle tuolille?
                     <br>3a. 危険な場所で__________。(遊ぶ あそぶ leikkiä)
                     <br><i>Kiken na basho de __________.</i>
                     <br>Ei saa leikkiä vaarallisissa paikoissa.
@@ -36,19 +39,19 @@ let harjoitukset = [{teht: `Lopussa vastaukset voi paljastaa klikkaamalla.
                     <br><i>Douro de __________.</i>
                     <br>Autotiellä ei saa juosta.
                     <br><br>Vastaukset:<br>`,
-                    vast: `1a. 宿題を終えてから遊びに行きます。
-                    <br>1b. コーヒーを飲んでからお風呂に入ります。
-                    <br>1c. 映画を見てから買い物に行きましょう。
+                    vast: `1a. 宿題を<strong>終えてから</strong>遊びに行きます。
+                    <br>1b. コーヒーを<strong>飲んでから</strong>お風呂に入ります。
+                    <br>1c. 映画を<strong>見てから</strong>買い物に行きましょう。
                     <br>
-                    <br>2a. 靴を脱いでもいいですよ。
-                    <br>2b. 写真を撮ってもいいですか。
-                    <br>2c. この椅子に座ってもいいですか。
+                    <br>2a. 靴を<strong>脱いでもいい</strong>ですよ。
+                    <br>2b. 写真を<strong>撮ってもいい</strong>ですか。
+                    <br>2c. この椅子に<strong>座ってもいい</strong>ですか。
                     <br>
                     <br>! いけません, いけない ja だめ ovat kaikki hyväksyttyjä
-                    <br>3a. 危険な場所で遊んではいけません。
-                    <br>3b. テスト中に携帯電話を使ってはいけません。
-                    <br>3c. ゴミを道に捨ててはいけません。
-                    <br>3d. 道路で走ってはいけません。`},
+                    <br>3a. 危険な場所で<strong>遊んではいけません</strong>。
+                    <br>3b. テスト中に携帯電話を<strong>使ってはいけません</strong>。
+                    <br>3c. ゴミを道に<strong>捨ててはいけません</strong>。
+                    <br>3d. 道路で<strong>走ってはいけません。</strong>`},
                 {teht: "Toistaiseksi tyhjää!",
             vast: "やばい =^O^="}]
 
